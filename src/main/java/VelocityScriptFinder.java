88376a7f1350f541eccb5d229fa87141723d1aa7
Egor <kazuare@mail.ru>
2018-08-25 02:04:32 +0700

    commit attempts to parse velocity gracefully

--- src/main/java/VelocityScriptFinder.java
+++ src/main/java/VelocityScriptFinder.java
@@ -1,18 +1,31 @@
+import org.apache.velocity.runtime.parser.*;
+import org.apache.velocity.runtime.parser.node.SimpleNode;
+
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class VelocityScriptFinder {
-    public static List<CoordRange> findVelocityScripts(String beforeEvaluation) {
+    public static List<CoordRange> findVelocityScripts(String beforeEvaluation, String afterEvaluation) throws ParseException {
         List<CoordRange> coords = new ArrayList<>();
+        CharStream charStream = new VelocityCharStream(new StringReader(beforeEvaluation), 0, 0);
+        Parser parser = new Parser(charStream);
 
-        Pattern pattern = Pattern.compile("(?<!#)#(?!#)[^\\s]+");
-        Matcher m = pattern.matcher(beforeEvaluation);
-        while(m.find()) {
-            coords.add(new CoordRange(m.start(), m.end()-1));
+        Token nextToken = parser.getNextToken();
+        while(nextToken.kind != ParserConstants.EOF) {
+            System.out.println(nextToken.toString());
+            System.out.println("==>" + nextToken.next);
+            System.out.println("==>" + nextToken.kind);
+            System.out.println("==>" + nextToken.specialToken);
+            System.out.println("==>" + nextToken.beginColumn + " " + nextToken.endColumn);
+            if (nextToken.specialToken != null) {
+                System.out.println(nextToken.specialToken.kind);
+                System.out.println(nextToken.specialToken.next);
+                System.out.println(nextToken.specialToken.specialToken);
+            }
+            System.out.println("--------");
+            nextToken = parser.getNextToken();
         }
-
         return coords;
     }
 }
--- src/test/java/VelocityScriptFinderTest.java
+++ src/test/java/VelocityScriptFinderTest.java
@@ -1,3 +1,4 @@
+import org.apache.velocity.runtime.parser.ParseException;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,25 +8,30 @@ import static org.junit.Assert.assertEquals;
 
 public class VelocityScriptFinderTest {
     @Test
-    public void searchTest(){
+    public void searchTest() throws ParseException {
         String before =
-                "#parse('aaa')\n" +
-                "#parse('bbb')\n" +
+                "#parse(\"aaa\")\n" +
+                "#parse(\"bbb\")\n" +
                 "ab cd ef \n" +
-                "#aaa('a')#bbb('b')\n" +
-                "abcd #bbb('b')\n";
+                "#aaa(\"a\")#bbb(\"b ) fs\"))\n" +
+                "abcd #bbb(\"b\")\n";
+
+        String after = "\n\n" +
+                "ab cd ef \n" +
+                "aresultbresult)\n" +
+                "abcd bresult\n";
 
         List<CoordRange> expected = new ArrayList<CoordRange>(){{
             add(new CoordRange(0,12));
             add(new CoordRange(14,26));
-            add(new CoordRange(38,55));
-            add(new CoordRange(62,70));
+            add(new CoordRange(38,60));
+            add(new CoordRange(68,76));
         }};
 
         for (CoordRange item: expected) {
             System.out.println(before.substring(item.a(), item.b() + 1));
         }
-        assertEquals(expected, VelocityScriptFinder.findVelocityScripts(before));
+        assertEquals(expected, VelocityScriptFinder.findVelocityScripts(before, after));
     }
 
 }
